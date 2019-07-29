58847ca9bf5a104d81a7556596d9f890d68874de
Renee France <renee5.0@outlook.com>
2019-07-29 04:52:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,8 @@ def long_planeteer_calls
 end
 
 def find_the_cheese(array)# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |i|
   cheese_types.include?(i)
-  
+  end
 end
