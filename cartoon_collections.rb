5a7e561f49f9812065576accf334932415232eec
Renee France <renee5.0@outlook.com>
2019-07-29 04:05:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def long_planeteer_calls(calls_long, calls_short) # code an argument here
   call_long.["axe", "earth", "wind", "fire"]all? do |call| # Your code here
-    call.length < 4?
+    if call.length < 4? 
   end
 end
 
