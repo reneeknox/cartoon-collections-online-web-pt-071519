84672cf00e0dbdfde1343d6b52f7c321dcf83e7d
Renee France <renee5.0@outlook.com>
2019-07-29 03:47:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet# code an argument here
 end
 
 def long_planeteer_calls(call_long) # code an argument here
-  call_long = [].all? do |call| # Your code here
+  call_long.all? do |call| # Your code here
     call.length < 4?
   end
 end
