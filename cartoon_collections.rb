375b4c1af45952085f67b4f7acc769510f6f8116
Renee France <renee5.0@outlook.com>
2019-07-29 03:37:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,11 @@ def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
+def summon_captain_planet# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call_s# code an argument here
   # Your code here
 end
 
