8470f8e0d2829e9331e0c29664add9c048d3dd7c
Renee France <renee5.0@outlook.com>
2019-07-29 04:03:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,6 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long, calls_short) # code an argument here
-  calls_long = ["axe", "earth", "wind", "fire"].all? do |call|
-    
-    call.long?
-  calls_short = ["wind", "fire", "tree", "axe", "code"]
   call_long.all? do |call| # Your code here
     call.length < 4?
   end
