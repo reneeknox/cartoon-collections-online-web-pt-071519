a90f93bab93ce0bda6478dca34c53d16823ff407
Renee France <renee5.0@outlook.com>
2019-07-29 03:38:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls(call_s# code an argument here
-  # Your code here
+def long_planeteer_calls(calls) # code an argument here
+  calls_long = true # Your code here
 end
 
 def find_the_cheese# code an argument here
